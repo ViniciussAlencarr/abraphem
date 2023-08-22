383175309084217ef97c0e772060743bd8f24e7d
Vinicius Alencar <vini@liderprojetos.me>
2023-08-22 11:00:14 -0300

    subindo alterações

--- src/components/admin/AnalizeOpenManifest.admin.tsx
+++ src/components/admin/AnalizeOpenManifest.admin.tsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react"
 import { MdOutlineKeyboardBackspace } from 'react-icons/md'
 import { ToastContainer, toast } from 'react-toastify'
+
 import api from "../../services/api"
 
 import { ManifestRequest } from '../../types/Manifest'
+import { User } from '../../types/User'
 
 import '../css/admin/AnalizeOpenManifest.admin.css'
 
 import { GetUserName } from './GetUserName.admin'
 
 import arrowIcon from '../../assets/fa-solid_check.svg'
+import { SeeDetailsOfUser } from "./SeeDetailsOfUser.admin"
 
 export const AnalizeOpenManifestComponent = (params: {
     manifest: ManifestRequest,
@@ -19,7 +22,33 @@ export const AnalizeOpenManifestComponent = (params: {
 }) => {
     let date = new Date();
     const [response, setResponse] = useState(params.manifest.response.value)
-
+    const [open, setOpen] = useState(false)
+    const [user, setUser] = useState<User>({
+        id: "",
+        document: "",
+        typeDocument: "",
+        username: "",
+        fullName: "",
+        dateOfBirth: "",
+        state: "",
+        city: "",
+        gender: "",
+        race: "",
+        category: "",
+        typeOfPhone: "",
+        phoneNumber: "",
+        ownerName: "",
+        typeOfCoagulopathy: "",
+        severityOfCoagulopathy: "",
+        callCenterLocation: "",
+        password: "",
+        pcd: false,
+        typeOfDisability: "",
+        email: "",
+        roleUser: "",
+        profilePictureURL: ""
+    })
+    
     useEffect(() => {
     }, [])
 
@@ -31,9 +60,11 @@ export const AnalizeOpenManifestComponent = (params: {
         )
     }
     
-    const openUserInfo = () => {
+    const openUserInfo = async () => {
         try {
-            /* TODO: fazer chamada da request de visualização do usuário passando o id de 'params' */
+            const { data } = await api.get(`user/${params.manifest.userId}`)
+            setUser(data)
+            setOpen(true)
         } catch (err) {
             console.log(err)
         }
@@ -81,75 +112,81 @@ export const AnalizeOpenManifestComponent = (params: {
         <div className="analize-open-manifests">
             {
                 params.open ? <>
-                    <div className="analize-open-manifests-header">
-                        <div className="back-btn-context" onClick={() => params.setOpen(!open)}>
-                            <MdOutlineKeyboardBackspace size={25} />
-                            <button className='back-btn' onClick={() => params.setOpen(!open)}>Voltar</button>
-                        </div>
-                        <div className="manifest-type">
-                            <div className="manifest-type-context">
-                                <div className="icon-imgs">
-                                    {getSvg('#FFD600')}
-                                    <img className="img-icon arrow-icon" src={arrowIcon} />
+                    {
+                        !open ? <>
+                            <div className="analize-open-manifests-header">
+                                <div className="back-btn-context" onClick={() => params.setOpen(!params.open)}>
+                                    <MdOutlineKeyboardBackspace size={25} />
+                                    <button className='back-btn' onClick={() => params.setOpen(!params.open)}>Voltar</button>
                                 </div>
-                                <span className="type-label active">Em aberto</span>
-                            </div>
-                            <div className="line-marker" style={{ background: '#969696' }}></div>
-                            <div className="manifest-type-context">
-                                <div className="icon-imgs">
-                                    {getSvg('#969696')}
-                                    <img className="img-icon arrow-icon" src={arrowIcon} />
-                                </div>
-                                <span className="type-label">Em andamento</span>
-                            </div>
-                            <div className="line-marker" style={{ background: '#969696' }}></div>
-                            <div className="manifest-type-context">
-                                <div className="icon-imgs">
-                                    {getSvg('#969696')}
-                                    <img className="img-icon arrow-icon" src={arrowIcon} />
-                                </div>
-                                <span className="type-label">Concluido</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="manifest-content">
-                        <div className="requester-name">
-                            <span className="name">Solicitante: <GetUserName userId={params.manifest.userId} /></span>
-                        </div>
-                        <div className="more-info-about-user">
-                            <button className="more-info-btn" onClick={() => openUserInfo()}>Mais informações do usuário</button>
-                        </div>
-                        <div className="protocol_manifest">
-                            <div className="protocol">
-                                <span className="protocol-number">{params.manifest.protocol.value}</span>
-                                <span className="protocol-period">{params.manifest.protocol.period}</span>
-                            </div>
-                            <div className="manifest">
-                                <span className="manifest-title">{params.manifest.title}</span>
-                                <span className="manifest-description">{params.manifest.manifestValue}</span>
-                            </div>
-                        </div>
-                        <div className="answer-manifest">
-                            <div className="answer-manifest-header">
-                                <div className="wait-analize">
-                                    <div className="wait-analize-title">{params.manifest.response.state}</div>
-                                    <div className="wait-analize-period">{params.manifest.response.period}</div>
-                                </div>
-                                <div className="wait-analize-message">
-                                    {params.manifest.response.title}
+                                <div className="manifest-type">
+                                    <div className="manifest-type-context">
+                                        <div className="icon-imgs">
+                                            {getSvg('#FFD600')}
+                                            <img className="img-icon arrow-icon" src={arrowIcon} />
+                                        </div>
+                                        <span className="type-label active">Em aberto</span>
+                                    </div>
+                                    <div className="line-marker" style={{ background: '#969696' }}></div>
+                                    <div className="manifest-type-context">
+                                        <div className="icon-imgs">
+                                            {getSvg('#969696')}
+                                            <img className="img-icon arrow-icon" src={arrowIcon} />
+                                        </div>
+                                        <span className="type-label">Em andamento</span>
+                                    </div>
+                                    <div className="line-marker" style={{ background: '#969696' }}></div>
+                                    <div className="manifest-type-context">
+                                        <div className="icon-imgs">
+                                            {getSvg('#969696')}
+                                            <img className="img-icon arrow-icon" src={arrowIcon} />
+                                        </div>
+                                        <span className="type-label">Concluido</span>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="answer-context">
-                                <div className="answer-context-title">Resposta</div>
-                                <div className="answer-value-textarea">
-                                    <textarea onChange={event => setResponse(event.target.value)} className="answer-value" placeholder="Digite a resposta" value={response}/>
+                            <div className="manifest-content">
+                                <div className="requester-name">
+                                    <span className="name">Solicitante: <GetUserName userId={params.manifest.userId} /></span>
+                                </div>
+                                <div className="more-info-about-user">
+                                    <button className="more-info-btn" onClick={() => openUserInfo()}>Mais informações do usuário</button>
+                                </div>
+                                <div className="protocol_manifest">
+                                    <div className="protocol">
+                                        <span className="protocol-number">{params.manifest.protocol.value}</span>
+                                        <span className="protocol-period">{params.manifest.protocol.period}</span>
+                                    </div>
+                                    <div className="manifest">
+                                        <span className="manifest-title">{params.manifest.title}</span>
+                                        <span className="manifest-description">{params.manifest.manifestValue}</span>
+                                    </div>
+                                </div>
+                                <div className="answer-manifest">
+                                    <div className="answer-manifest-header">
+                                        <div className="wait-analize">
+                                            <div className="wait-analize-title">{params.manifest.response.state}</div>
+                                            <div className="wait-analize-period">{params.manifest.response.period}</div>
+                                        </div>
+                                        <div className="wait-analize-message">
+                                            {params.manifest.response.title}
+                                        </div>
+                                    </div>
+                                    <div className="answer-context">
+                                        <div className="answer-context-title">Resposta</div>
+                                        <div className="answer-value-textarea">
+                                            <textarea onChange={event => setResponse(event.target.value)} className="answer-value" placeholder="Digite a resposta" value={response}/>
+                                        </div>
+                                    </div>
+                                    <div className="send-response">
+                                        <button className="send-response-btn" onClick={sendResponse}>Enviar resposta</button>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="send-response">
-                                <button className="send-response-btn" onClick={sendResponse}>Enviar resposta</button>
-                            </div>
-                        </div>
-                    </div>
+                        </>
+                        :
+                        <SeeDetailsOfUser user={user} setOpen={setOpen} open={open} />
+                    }
                 </>
                 : <></>
             }
