8bc9764e0dd76de7be9e48ab9c8f866499731193
Vinicius Alencar <vini@liderprojetos.me>
2023-11-15 04:30:44 -0300

    feat:

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://xeqdpos3al.execute-api.us-east-1.amazonaws.com/dev'
+  baseURL: 'https://aagiab9575.execute-api.us-east-1.amazonaws.com/dev'
   //baseURL: 'http://localhost:3000/dev'
 });
 
